e50050896dc97abf65474bfbd098f42a1e6383b6
JangJaeHun <129411490+l-wanderer01@users.noreply.github.com>
2026-08-03 16:30:52 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 93.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -4,28 +4,30 @@ import java.lang.Math;
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        
-        // brown + yellow = return[0] * return[1]
-        long total = brown + yellow;
-        
-        ArrayList<Integer> al = new ArrayList<>();
-        
-        // 약수 구하기
-        for (int i = 3; i <= Math.sqrt(total); i++) {
-            if (total % i == 0) al.add(i); 
-        }
-        
-        // 사이드를 모두 brown으로 채웠을 때, yellow의 개수만큼 남는지
-        
-        for (int i = 0; i < al.size(); i++) {
-            long height = al.get(i), width = total / al.get(i);
-            if (width * 2 + (height - 2) * 2 == brown) {
-                answer[0] = (int) width;
-                answer[1] = (int) height;
-                break;
+        // 격자 개수
+        int total = brown + yellow;
+        for (int i = 1; i <= Math.sqrt(total); i++){
+            if (total % i == 0) {
+                int row = i;
+                int col = total/i;
+                if (yellow == countYellow(row,col)) {
+                    answer[0] = row;
+                    answer[1] = col;
+                    break;
+                }   
             }
         }
-        
+        // 가로
+        int max = Math.max(answer[0], answer[1]);
+        // 세로
+        int min = Math.min(answer[0], answer[1]);
+        answer[0] = max;
+        answer[1] = min;
         return answer;
+    }
+    
+    // 노란 카펫 개수
+    public int countYellow(int a, int b) {
+        return (a-2)*(b-2);
     }
 }

720ceeb9a68ce064dac36df366996e1ca6f610b7
JangJaeHun <129411490+l-wanderer01@users.noreply.github.com>
2026-08-04 11:02:57 +0900

    [level 2] Title: 기능개발, Time: 0.80 ms, Memory: 85.8 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -11,10 +11,12 @@ class Solution {
         // queue 값 초기화
         for (int i = 0; i < len; i++) {
             int cnt = 0;
+            // 남은 작업일 수 계산
             while(progresses[i] < 100) {
                 progresses[i] += speeds[i];
                 cnt++;
             }
+            // 위의 코드를 수학적으로 풀면 ((100-progresses[i])/speeds[i])를 올림한 것 (Math.ceil)
             // 남은 작업 일수를 queue에 넣음
             queue.add(cnt);
         }
@@ -24,12 +26,14 @@ class Solution {
             // 가장 우선순위가 높은 작업의 남은 일수를 꺼냄
             int target = queue.poll();
             int cnt = 1;
+            // queue가 비어있지 않고, queue의 최상단 값이 현재 작업한 일수보다 작은지
             while(!queue.isEmpty() && queue.peek() <= target) {
                 queue.poll();
                 cnt++;
             }
             answer.add(cnt);
         }
+        
         int size = answer.size();
         int[] result = new int[size];
         for (int i = 0; i < size; i++) {

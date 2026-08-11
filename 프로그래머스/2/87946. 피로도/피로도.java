cdc1b847e74048980c46774513be9715d66aa41c
JangJaeHun <129411490+l-wanderer01@users.noreply.github.com>
2026-08-11 17:55:38 +0900

    [level 2] Title: 피로도, Time: 1.78 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -2,33 +2,33 @@ import java.util.*;
 
 class Solution {
     int answer = -1;
-    
     public int solution(int k, int[][] dungeons) {
+        
+        // 현재 체력
+        int hp = k;
+        // 방문여부 체크
         boolean[] visited = new boolean[dungeons.length];
         
-        // dfs 하나가 알아서 모든 시작점을 순회합니다.
-        dfs(dungeons, 0, k, visited); 
+        // 모든 경우의 수 조회
+        dfs(dungeons, visited, hp, 0);
         
         return answer;
     }
     
-    public void dfs(int[][] dungeons, int cnt, int k, boolean[] visited) {
-        // 1. dfs가 호출될 때마다 최대 방문 횟수 갱신
+    // 완전탐색 (DFS)
+    public void dfs(int[][] dungeons, boolean[] visited, int hp, int cnt) {
         answer = Math.max(answer, cnt);
         
-        // 2. 모든 던전을 순회
         for (int i = 0; i < dungeons.length; i++) {
-            
-            // 아직 방문하지 않았고, 남은 피로도가 최소 필요 피로도보다 크거나 같다면
-            if (!visited[i] && k >= dungeons[i][0]) {
-                
-                visited[i] = true; // 방문 처리
-                
-                // 깊이 탐색 (상태값을 직접 변경하지 않고 연산 결과를 넘김)
-                dfs(dungeons, cnt + 1, k - dungeons[i][1], visited);
-                
-                visited[i] = false; // 방문 복구 (백트래킹)
+            if (!visited[i] && dungeons[i][0] <= hp) {
+                visited[i] = true;
+                dfs(dungeons, visited, hp - dungeons[i][1], cnt+1);
+                visited[i] = false;
             }
         }
     }
 }
+
+// 알고리즘 : DFS
+// 자료구조 : 배열
+// 조건 : 지금의 체력이 던전 입장 가능한지

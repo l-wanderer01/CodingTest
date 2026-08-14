34d3778b1a686396ca0dc791bcf2c75c421b3860
JangJaeHun <129411490+l-wanderer01@users.noreply.github.com>
2026-08-14 11:54:37 +0900

    [level 2] Title: 피로도, Time: 1.58 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -2,27 +2,43 @@ import java.util.*;
 
 class Solution {
     int answer = -1;
+    int hp;
+    boolean[] visited;
+    int[][] dungeons;
     public int solution(int k, int[][] dungeons) {
         
         // 현재 체력
-        int hp = k;
+        this.hp = k;
         // 방문여부 체크
-        boolean[] visited = new boolean[dungeons.length];
-        
+        visited = new boolean[dungeons.length];
+        this.dungeons = dungeons; // 동일한 변수명이기에 this로 지정
         // 모든 경우의 수 조회
-        dfs(dungeons, visited, hp, 0);
+        dfs(hp, 0);
         
         return answer;
     }
     
     // 완전탐색 (DFS)
-    public void dfs(int[][] dungeons, boolean[] visited, int hp, int cnt) {
-        answer = Math.max(answer, cnt);
+    public void dfs(int hp, int cnt) {
+        if (answer < cnt) {
+            answer = cnt;
+        }
+        // answer = Math.max(answer, cnt); // 위의 코드와 동일하게 동작
+        
+        // 모든 던전을 다 도는 경우를 찾았다면 그 즉시 종료
+        if (dungeons.length == cnt) {
+            answer = cnt;
+            return;
+        }
         
         for (int i = 0; i < dungeons.length; i++) {
             if (!visited[i] && dungeons[i][0] <= hp) {
                 visited[i] = true;
-                dfs(dungeons, visited, hp - dungeons[i][1], cnt+1);
+                // int currentHp = hp - dungeons[i][1];
+                // int currentCnt = cnt+1;
+                // dfs(currentHp, currentCnt);
+                dfs(hp-dungeons[i][1], cnt+1);
+                
                 visited[i] = false;
             }
         }

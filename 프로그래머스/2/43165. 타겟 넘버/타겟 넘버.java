deedc8d3a155aeb9196a76247cbcb29abcd7b618
JangJaeHun <129411490+l-wanderer01@users.noreply.github.com>
2026-08-14 21:31:04 +0900

    [level 2] Title: 타겟 넘버, Time: 6.78 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,34 +1,25 @@
-import java.util.*;
-
 class Solution {
-    static int tg;
-    static int answer = 0;
-    
+    int[] numbers;
+    int target;
+    int answer;
     public int solution(int[] numbers, int target) {
-        tg = target;
-        // 자료구조 = numbers로 주는 리스트 활용
-        // 알고리즘 = DFS
+        this.numbers = numbers;
+        this.target = target;
         
-        dfs(numbers, 0, numbers[0], 0);
-        dfs(numbers, 0, -1 * numbers[0], 0);
+        findTargetNumber(0, 0);
         
         return answer;
     }
     
-    public void dfs(int[] numbers, int idx, int num, int sum) {
-        // 종료조건
-        if (idx == numbers.length - 1) {
-            sum += num;
-            if (sum == tg) {
-                answer++;
-                System.out.println(answer);
-            }
-            return;   
+    public void findTargetNumber(int depth, int sum) {
+        if (depth == numbers.length) {
+            if (sum == target) answer++;
+            return;
         }
-        // 방문 노드 stack에 추가
-        sum += num;
-        dfs(numbers, idx+1, -1 * numbers[idx+1], sum);
-        dfs(numbers, idx+1, 1 * numbers[idx+1], sum);
         
+        // 해당 depth에서 값을 더하는 경우
+        findTargetNumber(depth+1, sum+numbers[depth]);
+        // 해당 depth에서 값을 빼는 경우
+        findTargetNumber(depth+1, sum-numbers[depth]);
     }
 }
